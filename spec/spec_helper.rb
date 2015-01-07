6248acf85d60c0a640e86ee2a962959d44f32e03
Guido Marucci Blas <guidomb@gmail.com>
2015-01-07 19:27:30 -0300

    Changes the order in which code is required in spec_helper.rb
    Application code is required after CodeClimate's test
    reporter in order to make it work.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,11 @@
 require 'bundler/setup'
 Bundler.setup
 
-require 'xbuild'
 require 'byebug'
 require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
+
+require 'xbuild'
 
 RSpec.configure do |config|
   # some (optional) config here
